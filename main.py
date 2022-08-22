c487e668ac26ae7fe9b4870c6ad453f71eae1883
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-22 23:32:26 +0530

    Add 6th command

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from rich.table import Table
 import threading
 import yfinance as yf
 import mplfinance as mpf
+import json
+from datetime import datetime
+import pandas as pd
 ###################################
 
 app = typer.Typer(rich_markup_mode="rich",help="[italic]It's an open-source tool that uses [green]Yahoo's[/green] publicly available APIs,and is intended for [red]research[/red] and [red]educational[/red] purposes.[/italic]")
@@ -158,6 +161,62 @@ def splits(market:str = typer.Argument(...,help="[italic blue]Enter required mar
     t2.start()
     t1.join()
 
+@app.command(help="[bold yellow]Show financials.[/bold yellow]")
+def finance(market:str = typer.Argument(...,help="[italic blue]Enter required market[/italic blue]"),quater:bool = typer.Option(default=False,help="get quarterly_financials")):
+    def main():
+        ticker = yf.Ticker(market.upper())
+        df = ticker.splits.reset_index()
+        if df.empty:
+            print(f"[yellow][bold]Sorry[/bold] ,unrecognized market:[bold]'{market}'[/bold][/yellow]")
+            typer.Exit()
+        else:
+            # Create a dataframe (we can't use this `df` DataFrame because of column names are change)
+            df = ticker.financials.reset_index() if quater else ticker.quarterly_financials.reset_index()
+            # DataFrame -> Json -> Dict
+            data = json.loads(df.to_json())
+            # Create `headers` List & `finance` Dict
+            headers, finance = ['Attribute'], {}
+            # Update `headers` List or Loop `data` keys
+            for key in data.keys():
+                if key.endswith('000'):
+                    key = int(key)
+                    key /= 1000
+                    headers.append(datetime.utcfromtimestamp(int(key)).strftime('%Y-%m-%d'))
+            # _____Optinal______
+            # Declare and Initialize variable `i`
+            i = 0
+            # Loop `data` values 
+            for value in data.values():
+                dt = []
+                for val in value.values():
+                    dt.append(str(val))
+                finance.update({headers[i]:dt})
+                i+=1
+            # Override `df` variable 
+            df = pd.DataFrame(finance)
+            # _____Optinal_______
+            # Create `console` instance 
+            console = Console()
+            # Create a table instance 
+            table = Table(headers[0],headers[1],headers[2],headers[3],headers[4])
+            # Loop `df` DateFrame
+            for index, row in df.iterrows():
+                table.add_row(str(row[headers[0]]),str(row[headers[1]]),str(row[headers[2]]),str(row[headers[3]]),str(row[headers[4]]),end_section=True)
+            # Print Rich's table
+            console.print(table)
+    def spinner():
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            transient=True,
+        ) as progress:
+            progress.add_task(description="Processing...", total=None)
+            time.sleep(5)
+    t1 = threading.Thread(target=main, args=())
+    t2 = threading.Thread(target=spinner, args=())
+    t1.start()
+    t2.start()
+    t1.join()
 
 
 if __name__ == "__main__":
