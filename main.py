ba5f712ab6064c31745d217db0b6f0fac6e2c3e2
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-22 21:25:40 +0530

    Add 2nd command

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ import random
 import httpx
 import time
 from rich import print
-from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.progress import Progress,SpinnerColumn, TextColumn
+from rich.console import Console
+from rich.table import Table
 import threading
+import yfinance as yf
 ###################################
 
 app = typer.Typer(rich_markup_mode="rich",help="[italic]It's an open-source tool that uses [green]Yahoo's[/green] publicly available APIs,and is intended for [red]research[/red] and [red]educational[/red] purposes.[/italic]")
 
 views = ['trending-tickers','most-active','gainers','losers']
 
-@app.command(help="[yellow]This command fetches the required market type.[/yellow]")
+@app.command(help="[bold yellow]fetches the market.[/bold yellow]")
 def fetch(view:str = typer.Argument(...,help="[italic blue]'trending-tickers','most-active','gainers','losers'[/italic blue]")):
     if view not in views:
         print(f"[red]Error: unrecognized arguments [bold]'{view}'[/bold]. The view should be [blue]'trending-tickers', 'most-active','gainers','losers'[/blue].[/red]")
         typer.Exit()
     else:
-        def api_call():
+        def main():
             url = f'https://yfinance-stocks.deta.dev/{view}'
             data = httpx.get(url).json()
             for dt in data:
@@ -32,15 +35,39 @@ def fetch(view:str = typer.Argument(...,help="[italic blue]'trending-tickers','m
             ) as progress:
                 progress.add_task(description="Processing...", total=None)
                 time.sleep(2)
-        t1 = threading.Thread(target=api_call, args=())
+        t1 = threading.Thread(target=main, args=())
         t2 = threading.Thread(target=spinner, args=())
         t1.start()
         t2.start()
         t1.join()
     
-@app.command(help="Say hello")
-def hello(name:str = typer.Argument(...,help="Enter your name.")):
-    print(f"Hello, {name}")
+@app.command(help="[bold yellow]get stock information.[/bold yellow]")
+def info(market:str = typer.Argument(...,help="[italic blue]Enter required market[/italic blue]")):
+    def main():
+        ticker = yf.Ticker(market.upper())
+        info = ticker.info
+        exist = True if info['regularMarketPrice'] != None else False
+        if exist:
+            console = Console()
+            table = Table("Name", "Value")
+            for k,v in info.items():
+                table.add_row(k,str(v),end_section=True)
+            console.print(table)
+        else:
+            print(f"[yellow][bold]Sorry[/bold] ,unrecognized market:[bold]'{market}'[/bold][/yellow]")
+    def spinner():
+        with Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            transient=True,
+        ) as progress:
+            progress.add_task(description="Processing...", total=None)
+            time.sleep(5)
+    t1 = threading.Thread(target=main, args=())
+    t2 = threading.Thread(target=spinner, args=())
+    t1.start()
+    t2.start()
+    t1.join()
 
 if __name__ == "__main__":
     app()
