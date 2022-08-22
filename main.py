28108d24278b9cea8882cedcc61d58cb666fdb3d
hasindusithmin <hasindusithmin64@gmail.com>
2022-08-22 20:29:43 +0530

    Add first command

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import typer
-
+import random
+import httpx
+import time
+from rich import print
+from rich.progress import Progress, SpinnerColumn, TextColumn
+import threading
 ###################################
 
 app = typer.Typer(rich_markup_mode="rich",help="[italic]It's an open-source tool that uses [green]Yahoo's[/green] publicly available APIs,and is intended for [red]research[/red] and [red]educational[/red] purposes.[/italic]")
 
+views = ['trending-tickers','most-active','gainers','losers']
+
+@app.command(help="[yellow]This command fetches the required market type.[/yellow]")
+def fetch(view:str = typer.Argument(...,help="[italic blue]'trending-tickers','most-active','gainers','losers'[/italic blue]")):
+    if view not in views:
+        print(f"[red]Error: unrecognized arguments [bold]'{view}'[/bold]. The view should be [blue]'trending-tickers', 'most-active','gainers','losers'[/blue].[/red]")
+        typer.Exit()
+    else:
+        def api_call():
+            url = f'https://yfinance-stocks.deta.dev/{view}'
+            data = httpx.get(url).json()
+            for dt in data:
+                for k,v in dt.items():
+                    x = ' ' * (10 - len(k) + 2)
+                    print(f'[bold blue]{k}[/bold blue]{x}[yellow]{v}[/yellow]')
+        def spinner():
+            with Progress(
+                SpinnerColumn(),
+                TextColumn("[progress.description]{task.description}"),
+                transient=True,
+            ) as progress:
+                progress.add_task(description="Processing...", total=None)
+                time.sleep(2)
+        t1 = threading.Thread(target=api_call, args=())
+        t2 = threading.Thread(target=spinner, args=())
+        t1.start()
+        t2.start()
+        t1.join()
+    
+@app.command(help="Say hello")
+def hello(name:str = typer.Argument(...,help="Enter your name.")):
+    print(f"Hello, {name}")
 
 if __name__ == "__main__":
     app()
